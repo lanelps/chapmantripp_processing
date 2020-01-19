846d967b65797b7290c09c2517a2cc0f72431a0a
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-01-20 11:07:53 +1300

    added wepack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
+	devServer: {
+		contentBase: './dist',
+	},
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
